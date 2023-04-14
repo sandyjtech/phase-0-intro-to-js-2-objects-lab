5e866b9116dcc8465deb7347020df7160db7a68b
Sandra <sandyjtech@gmail.com>
2023-04-14 10:27:21 -0700

    added objects

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streeAddress: "11 Broadway"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return {...employee, [key] : value};
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+ employee[key] = value;
+ return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    return {...employee[ delete employee.key]};
+        
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+}
